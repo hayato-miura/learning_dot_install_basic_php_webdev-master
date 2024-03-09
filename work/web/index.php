38bdcc5e5f0eb074bbf42662232e5c513b360f16
hayato-miura <miura.miura.515@gmail.com>
2024-03-09 13:38:47 +0900

    cookieの処理をやりました

---
+++ work/web/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require('../app/functions.php');
+
+// $color = filter_input(INPUT_COOKIE, 'color') ?? 'transparent';
+
+include('../app/_parts/_header.php');
+
+?>
+
+  <form action="result.php" method="get">
+    <label><input type="radio" name="color" value="orange"> Orange</label>
+    <label><input type="radio" name="color" value="pink"> Pink</label>
+    <label><input type="radio" name="color" value="gold"> Gold</label>
+    <button>Send</button>
+  </form>
+
+<?php
+
+include('../app/_parts/_footer.php');
