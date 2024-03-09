7cc08ae138f69a4c4e1e889511520ae75e39d22d
hayato-miura <miura.miura.515@gmail.com>
2024-03-09 14:23:22 +0900

    掲示板として最低限の必要な機能を実装しました

--- work/web/result.php
+++ work/web/result.php
@@ -1,11 +1,20 @@
 <?php
 
 require('../app/functions.php');
+$message = trim(filter_input(INPUT_POST,'message'));
+$message = $message !== '' ? $message : '...';
+
+$filename = '../app/messages.txt';
+$fp = fopen($filename, 'a');
+fwrite($fp, $message . "\n");
+fclose($fp);
+
 
 include('../app/_parts/_header.php');
 
 ?>
 
+  <p>Message added!</p>
   <p><a href="index.php">Go back</a></p>
 
 <?php
